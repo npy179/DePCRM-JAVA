353bab468353c81dcb3ee91c5d25730d0500cc6b
npy179 <npy179@gmail.com>
2015-01-27 22:14:11 -0500

    modify the bug startpoint less than 0

--- DePRCM.java
+++ DePRCM.java
@@ -41,8 +41,8 @@ public class DePRCM{
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
             String line = null;
-            String title1 = br.readLine();String titlenew = "Bed format";bw.write(titlenew);bw.newLine();
-            String title2 = br.readLine();bw.write(title2);bw.newLine();
+            String title1 = br.readLine();String titlenew = "Bed format";//bw.write(titlenew);bw.newLine();
+            String title2 = br.readLine();//bw.write(title2);bw.newLine();
 
 
 	    while((line = br.readLine())!=null){
@@ -62,15 +62,22 @@ public class DePRCM{
 		    if((EndPoint-StartPoint)<5000){
 			double MidPoint = Math.floor((StartPoint+EndPoint)/2);
 
-			double EstartPoint = MidPoint - 1499;
-			double EendPoint = MidPoint + 1501;
+			double dEstartPoint = MidPoint - 1499;
+			double dEendPoint = MidPoint + 1501;
+			
+			if(dEstartPoint<0){
+			    dEstartPoint = 0;
+			}
 
-			tokens[3] = Double.toString(EstartPoint);
-			tokens[4] = Double.toString(EendPoint);
+			int EstartPoint = (int)dEstartPoint;
+			int EendPoint = (int)dEendPoint;
 
+			tokens[3] = Integer.toString(EstartPoint);
+			tokens[4] = Integer.toString(EendPoint);
 
+			String name = "chr"+tokens[0];
 
-			String Exline = tokens[0]+"\t"+tokens[1]+"\t"+tokens[2]+"\t"+tokens[3]+"\t"+tokens[4]+"\n";
+			String Exline = name+"\t"+tokens[3]+"\t"+tokens[4]+"\t"+tokens[5]+"\t"+tokens[6]+"\n";
 			//			System.out.println(Exline);
 			bw.write(Exline);
 
